aff8853482eb6829054647ff0ae89a3f3021be7e
Jeremias Ramos <Exzed13@hotmail.com>
2023-03-08 21:48:20 -0300

    Desafio manejo de archivos completado

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
 import express from 'express';
-
-//const productManager = require('./productManager.js');
+import ProductManager from './productManager.js';
+//const productManager = require('./productManager');
 
 const app = express();
 
 const PORT = 8080;
+const products = new ProductManager('./files/Products.json');
 
-//const products = new productManager('./files/Products.json');
-
-app.get('/',(req, res) => {
-    //const productos = await products.getProducts();
-    res.send('productos');
+app.get('/products', async (req, res) => {
+    const productos = await products.getProducts();
+    res.send(productos);
 });
 
 //server.on('error', err => console.log(`error: ${err}`));
 
+// const server = 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`server listening on PORT ${PORT}`);
 });
--- src/index.js
+++ src/index.js
@@ -1,61 +1,64 @@
-//import ProductManager from "./productManager.js";
+import ProductManager from "./productManager.js";
 
-//const manager = new ProductManager();
+const manager = new ProductManager();
 
-// const env = async () => {
-//     let firstGet = await manager.getProducts();
-//     //console.log(firstGet);
+const env = async () => {
+    let firstGet = await manager.getProducts();
+    console.log(firstGet);
 
-//     const product1 = {
-//         title: "Mantecol",
-//         description: "Tamaño familiar",
-//         price: 600,
-//         thumbnail: "No Image",
-//         code: 123,
-//         stock: 23,
-//     };
-//     const product2 = {
-//         title: "Mantecol",
-//         description: "Tamaño familiar",
-//         price: 600,
-//         thumbnail: "No Image",
-//         code: 456,
-//         stock: 23,
-//     };
-//     const product3 = {
-//         title: "Mantecol",
-//         description: "Tamaño familiar",
-//         price: 600,
-//         thumbnail: "No Image",
-//         code: 789,
-//         stock: 23,
-//     };
-//     const product4 = {
-//         title: "Mantecol",
-//         description: "Tamaño familiar",
-//         price: 600,
-//         thumbnail: "No Image",
-//         code: 654,
-//         stock: 23,
-//     };
+    const product1 = {
+        title: "Mantecol",
+        description: "Tamaño familiar",
+        price: 600,
+        thumbnail: "No Image",
+        code: 123,
+        stock: 23,
+    };
+    const product2 = {
+        title: "Mantecol",
+        description: "Tamaño familiar",
+        price: 600,
+        thumbnail: "No Image",
+        code: 456,
+        stock: 23,
+    };
+    const product3 = {
+        title: "Mantecol",
+        description: "Tamaño familiar",
+        price: 600,
+        thumbnail: "No Image",
+        code: 789,
+        stock: 23,
+    };
+    const product4 = {
+        title: "Mantecol",
+        description: "Tamaño familiar",
+        price: 600,
+        thumbnail: "No Image",
+        code: 654,
+        stock: 23,
+    };
 
-//     let addProduct = await manager.addProduct(product1);
-//     let addProduct1 = await manager.addProduct(product2);
-//     let addProduct2 = await manager.addProduct(product3);
-//     console.log(addProduct2);
-//     let addProduct3 = await manager.addProduct(product4);
 
-//     let getProducts = await manager.getProducts();
-//     console.log(getProducts);
+    //TESTING
 
-//     let getProductById = await manager.getProductsById(3);
-//     console.log(getProductById);
+    let addProduct = await manager.addProduct(product1);
+    let addProduct1 = await manager.addProduct(product2);
+    let addProduct2 = await manager.addProduct(product3);
+    console.log(addProduct2);
+    let addProduct3 = await manager.addProduct(product4);
 
-//     let updatedProduct = await manager.updateProduct(2, "nutella", "500gr", 1200, "No image", 153, 12);
-//     console.log(updatedProduct)
+    let getProducts = await manager.getProducts();
+    console.log(getProducts);
 
-//     let deleteProduct = await manager.deleteProduct(3);
-//     console.log(deleteProduct);
-// };
+    let getProductById = await manager.getProductsById(3);
+    console.log(getProductById);
 
-//env();
+    let updatedProduct = await manager.updateProduct(2, "nutella", "500gr", 1200, "No image", 153, 12);
+    console.log(updatedProduct)
+
+    let deleteProduct = await manager.deleteProduct(3);
+    console.log(deleteProduct);
+};
+
+env();
